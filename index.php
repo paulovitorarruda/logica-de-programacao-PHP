a0b3f99695fe0feead6f6cc0a42e7c80a84318de
paulovitorarruda <paulovitorparruda@gmail.com>
2025-10-03 21:25:34 -0300

    Aula 2 PHP
    Exercício prático #2 - PHP

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?php
-//Código PHP!
-//HelloWorld!
-$HelloWorld = ['H','-','e','-','l','-','l','-','o','-','W','-','o','-','r','-','l','-','d',];
-$string = '';
-for($i = 0; $i < count($HelloWorld); $i++){
-	if($HelloWorld[$i] == '-'){
-		continue;
-	}    
-	$string.=$HelloWorld[$i];
+//1) Verificar se tem mais de 5 letras.
+//2) Se é número.
+//3) Se possui o @.
+
+//isset = Verificar se existe = Variável ter valor ou ser criada.
+if(isset($_POST['acao'])){
+	$nome = $_POST['nome'];
+	$numero = $_POST['numero'];
+	$email = $_POST['email'];
+	if(strlen($nome) >= 5){
+		echo 'Nosso nome tem mais ou igual a 5 letras!';
+		echo '<br>';
+	}
+
+	if(is_numeric($numero)){
+		echo 'É número!';
+		echo '<br>';
+	}
+
+	if(strstr($email,'@') != ''){
+		echo 'Tem e-mail!';
+		echo '<br>';
+		if(strstr($email, 'gmail') != ''){
+			echo 'O e-mail do usuário é gmail';
+			echo '<br>';
+		}
+	}
+
 }
-echo $string;
 ?>
+<form method="post">
+	<input type="text" name="nome">
+	<input type="text" name="numero">
+	<input type="text" name="email">
+	<input type="submit" name="acao">
+</form>
